6ca8990ffc4daf9eddf6d455d5b2de9fb9130cf2
Salim Khozami <khozami.salim@gmail.com>
2021-06-09 15:34:48 +0200

    update all

--- extract_data_using_matplot.py
+++ extract_data_using_matplot.py
@@ -10,14 +10,11 @@ from matplotlib.collections import EventCollection
 data = sio.loadmat("Sandiego.mat")
 data3d = np.array(data["Sandiego"], dtype=float)
 
-# print(data)
-
+# normalise data set [0,1]
 for idx in range(400):
     norm = np.linalg.norm(data3d[idx][:][:])
     normal_array = data3d[idx][:][:]/norm
     data3d[idx][:][:] = normal_array
-
-#print(data3d[120,120,...]) 
 
 xdata = np.linspace(400, 2500, num=224)
 ydata = data3d[120,120,...].tolist()
@@ -26,25 +23,6 @@ ydata = data3d[120,120,...].tolist()
 fig = plt.figure()
 ax = fig.add_subplot(1, 1, 1)
 ax.plot(xdata, ydata, color='tab:blue')
-
-# create the events marking the x data points
-#xevents1 = EventCollection(xdata1, color='tab:blue', linelength=0.05)
-#xevents2 = EventCollection(xdata2, color='tab:orange', linelength=0.05)
-
-# create the events marking the y data points
-"""
-yevents1 = EventCollection(ydata1, color='tab:blue', linelength=0.05,
-                           orientation='vertical')
-yevents2 = EventCollection(ydata2, color='tab:orange', linelength=0.05,
-                           orientation='vertical')
-"""
-# add the events to the axis
-"""
-ax.add_collection(xevents1)
-ax.add_collection(xevents2)
-ax.add_collection(yevents1)
-ax.add_collection(yevents2)
-"""
 
 # set the limits
 ax.set_xlim([400, 2500])
@@ -58,10 +36,7 @@ ax.set_title("Signature spectrale d'un pixel")
 # display the plot
 plt.show()
 
-
-# create some y data points
-
-
+#trying to male mat file from numpy data
 # data3d.dtype
 #transformed_data3d = {'__header__': b'MATLAB 5.0 MAT-file, Platform: PCWIN64, Created on: Wed Nov 05 20:20:56 2014', '__version__': '1.0', '__globals__': [], 'Sandiego2': data3d}
 #norm = np.linalg.norm(data3d)
